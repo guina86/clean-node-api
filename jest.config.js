d80feb3198bd3390f6a21495752ef218b7d85f7d
Leandro Guina <guina86@gmail.com>
2022-05-10 20:57:59 -0300

    chore: add jest configs for mongodb

--- jest.config.js
+++ jest.config.js
@@ -1,23 +1,12 @@
 module.exports = {
-  // Indicates whether the coverage information should be collected while executing the test
+  roots: ['<rootDir>/src'],
   collectCoverage: true,
-
-  // An array of glob patterns indicating a set of files for which coverage information should be collected
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
-
-  // The directory where Jest should output its coverage files
   coverageDirectory: 'coverage',
-
-  // Indicates which provider should be used to instrument code for coverage
   coverageProvider: 'v8',
-
-  // A list of paths to directories that Jest should use to search for files in
-  roots: [
-    '<rootDir>/src'
-  ],
-
-  // A map from regular expressions to paths to transformers
+  preset: '@shelf/jest-mongodb',
+  testEnvironment: 'node',
   transform: {
-    '^.+\\.ts$': '@swc/jest'
+    '.+\\.ts$': 'ts-jest'
   }
 }
