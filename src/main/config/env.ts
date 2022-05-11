4b77c5156f4214a177014a0bbae17500d515866c
Leandro Guina <guina86@gmail.com>
2022-05-11 19:16:17 -0300

    feat: add SignUp Factory

---
+++ src/main/config/env.ts
@@ -0,0 +1,4 @@
+export default {
+  mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:49153/clean-node-api',
+  port: process.env.PORT || 5050
+}
--- src/main/server.ts
+++ src/main/server.ts
@@ -1,3 +1,9 @@
-import app from './config/app'
+import { MongoHelper } from '../infra/db/mongodb/helpers/mongo-helper'
+import env from '../main/config/env'
 
-app.listen(5050, () => console.log('Sever running at http://localhost:5050'))
+console.log(env.mongoUrl)
+MongoHelper.connect(env.mongoUrl)
+  .then(async () => {
+    const app = (await import('./config/app')).default
+    app.listen(5050, () => console.log(`Sever running at http://localhost:${env.port}`))
+  }).catch(console.error)
