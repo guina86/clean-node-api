855a77463e6510d2e38a83dd95f958d10f57360d
Leandro Guina <guina86@gmail.com>
2022-05-12 16:08:59 -0300

    refactor: make server error return the error stack

--- src/main/decorators/log.ts
+++ src/main/decorators/log.ts
@@ -6,7 +6,6 @@ export class LogControllerDecorator implements Controller {
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     const httpResponse = await this.controller.handle(httpRequest)
     if (httpResponse.statusCode === 500) { console.log('') }
-
     return httpResponse
   }
 }
