bcbf96d3701ba420c98f56ccccec845219821923
Leandro Guina <guina86@gmail.com>
2022-05-15 20:35:07 -0300

    feat: ensure AddSurveyController calls AddSurveyUsecase with correct values

--- src/presentation/controllers/add-survey/add-survey-controller.ts
+++ src/presentation/controllers/add-survey/add-survey-controller.ts
@@ -1,14 +1,16 @@
 import { badRequest } from '../../helpers/http/http-helper'
-import { Controller, HttpRequest, HttpResponse, Validation } from './add-survey-controller-protocols'
+import { AddSurvey, Controller, HttpRequest, HttpResponse, Validation } from './add-survey-controller-protocols'
 
 export class AddSurveyController implements Controller {
   constructor (
-    private readonly validation: Validation
+    private readonly validation: Validation,
+    private readonly addSurvey: AddSurvey
   ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     const error = this.validation.validate(httpRequest.body)
     if (error) return badRequest(error)
+    await this.addSurvey.add(httpRequest.body)
     return null
   }
 }
