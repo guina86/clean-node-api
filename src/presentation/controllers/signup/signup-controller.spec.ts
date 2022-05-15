d20872311808fdb5b5a810b76f63e39bb36b5cf2
Leandro Guina <guina86@gmail.com>
2022-05-15 11:20:45 -0300

    test: ensure SignUpController returns 500 if Authentication throws

--- src/presentation/controllers/signup/signup-controller.spec.ts
+++ src/presentation/controllers/signup/signup-controller.spec.ts
@@ -101,4 +101,14 @@ describe('SignUp Controller', () => {
     await sut.handle(makeFakeRequest())
     expect(auth).toHaveBeenCalledWith({ email: 'any_email@mail.com', password: 'any_password' })
   })
+
+  it('should return 500 if Authentication throws', async () => {
+    const sut = makeSut()
+    jest.spyOn(authenticationStub,'auth').mockImplementationOnce(async () => {
+      throw new Error()
+    })
+    const httpResponse = await sut.handle(makeFakeRequest())
+    expect(httpResponse.statusCode).toBe(500)
+    expect(httpResponse.body).toEqual(new ServerError())
+  })
 })
