21c016872d87823e2412b2857af4f911053dc3f1
Leandro Guina <guina86@gmail.com>
2022-05-15 11:17:20 -0300

    feat: ensure SignUpController calls Authentication with correct values

--- src/main/factories/signup/signup-factory.ts
+++ src/main/factories/signup/signup-factory.ts
@@ -1,3 +1,4 @@
+import env from '../../config/env'
 import { SignUpController } from '../../../presentation/controllers/signup/signup-controller'
 import { DbAddAccount } from '../../../data/usecases/add-account/db-add-account'
 import { BcryptAdapter } from '../../../infra/criptography/bcrypt-adapter/bcrypt-adapter'
@@ -6,13 +7,17 @@ import { LogMongoRepository } from '../../../infra/db/mongodb/log/log-mongo-repo
 import { LogControllerDecorator } from '../../decorators/log-controller-decorator'
 import { Controller } from '../../../presentation/protocols'
 import { makeSignUpValidation } from './signup-validation-factory'
+import { JwtAdapter } from '../../../infra/criptography/jwt-adapter/jwt-adapter'
+import { DbAuthentication } from '../../../data/usecases/authentication/db-authentication'
 
 export const makeSignUpController = (): Controller => {
   const bcryptAdapter = new BcryptAdapter()
   const accountMongorepository = new AccountMongorepository()
+  const jwtAdapter = new JwtAdapter(env.jwtSecret)
+  const dbAuthentication = new DbAuthentication(accountMongorepository, bcryptAdapter, jwtAdapter, accountMongorepository)
   const dbAddAccount = new DbAddAccount(bcryptAdapter, accountMongorepository)
   const validationComposite = makeSignUpValidation()
-  const signUpController = new SignUpController(dbAddAccount, validationComposite)
+  const signUpController = new SignUpController(dbAddAccount, validationComposite, dbAuthentication)
   const logMongoRepository = new LogMongoRepository()
   return new LogControllerDecorator(signUpController, logMongoRepository)
 }
--- src/presentation/controllers/signup/signup-controller.ts
+++ src/presentation/controllers/signup/signup-controller.ts
@@ -1,10 +1,11 @@
 import { badRequest, ok, serverError } from '../../helpers/http/http-helper'
-import { HttpRequest, HttpResponse, Controller, AddAccount, Validation } from './signup-controller-protocols'
+import { HttpRequest, HttpResponse, Controller, AddAccount, Validation, Authentication } from './signup-controller-protocols'
 
 export class SignUpController implements Controller {
   constructor (
     private readonly addAccount: AddAccount,
-    private readonly validation: Validation
+    private readonly validation: Validation,
+    private readonly authentication: Authentication
   ) { }
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
@@ -17,6 +18,7 @@ export class SignUpController implements Controller {
         email,
         password
       })
+      await this.authentication.auth({ email, password })
       return ok(account)
     } catch (error) {
       return serverError(error)
