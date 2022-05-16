44f2505d255776e5eb33e42d194871a1fddbf124
Leandro Guina <guina86@gmail.com>
2022-05-16 15:57:55 -0300

    test: ensure AccountMongoRepository find an account by token without role

--- src/infra/db/mongodb/account/account-mongo-repository.spec.ts
+++ src/infra/db/mongodb/account/account-mongo-repository.spec.ts
@@ -7,7 +7,8 @@ let accountCollection: Collection
 const makeFakeAccount = (): any => ({
   name: 'valid_name',
   email: 'valid_email@mail.com',
-  password: 'valid_password'
+  password: 'valid_password',
+  accessToken: 'valid_token'
 })
 
 describe('Account Mongo Repository', () => {
@@ -64,6 +65,25 @@ describe('Account Mongo Repository', () => {
       const account = await accountCollection.findOne({ _id: insertedId })
       expect(account).toBeTruthy()
       expect(account.accessToken).toBe('valid_token')
+    })
+  })
+
+  describe('loadByToken()', () => {
+    it('should return an account on loadByToken without role', async () => {
+      const sut = new AccountMongorepository()
+      await accountCollection.insertOne(makeFakeAccount())
+      const account = await sut.loadByToken('valid_token')
+      expect(account).toBeTruthy()
+      expect(account.id).toBeTruthy()
+      expect(account.name).toBe('valid_name')
+      expect(account.email).toBe('valid_email@mail.com')
+      expect(account.password).toBe('valid_password')
+    })
+
+    it('should return null if loadByEmail fails', async () => {
+      const sut = new AccountMongorepository()
+      const account = await sut.loadByEmail('valid_email@mail.com')
+      expect(account).toBeNull()
     })
   })
 })
