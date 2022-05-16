d40eaf92dbbe1075ef4fd350e7eea7275106c026
Leandro Guina <guina86@gmail.com>
2022-05-15 22:18:18 -0300

    test: ensure DbAddSurvey throws if AddSurveyRepository throws

--- src/data/usecases/add-survey/db-add-survey.spec.ts
+++ src/data/usecases/add-survey/db-add-survey.spec.ts
@@ -2,9 +2,7 @@ import { DbAddSurvey } from './db-add-survey'
 import { AddSurveyModel, AddSurveyRepository } from './db-add-survey-protocols'
 
 class AddSurveyRepositoryStub implements AddSurveyRepository {
-  async add (surveyData: AddSurveyModel): Promise<void> {
-
-  }
+  async add (surveyData: AddSurveyModel): Promise<void> { }
 }
 
 const addSurveyRepositoryStub = new AddSurveyRepositoryStub()
@@ -20,10 +18,17 @@ const makeSut = (): DbAddSurvey => new DbAddSurvey(addSurveyRepositoryStub)
 
 describe('DbAddSurvey Usecase', () => {
   it('should call AddSurveyRepository with correct values', async () => {
-    const addSpy = jest.spyOn(addSurveyRepositoryStub, 'add')
     const sut = makeSut()
+    const addSpy = jest.spyOn(addSurveyRepositoryStub, 'add')
     const surveyData = makeFakeSurveyData()
     await sut.add(surveyData)
     expect(addSpy).toHaveBeenCalledWith(surveyData)
+  })
+
+  it('should throw if AddSurveyRepository throws', async () => {
+    const sut = makeSut()
+    jest.spyOn(addSurveyRepositoryStub, 'add').mockRejectedValueOnce(new Error())
+    const promise = sut.add(makeFakeSurveyData())
+    await expect(promise).rejects.toThrow()
   })
 })
