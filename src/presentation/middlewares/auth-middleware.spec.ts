c618cddb0956f6e00eecd5c4c245f5894d247b9a
Leandro Guina <guina86@gmail.com>
2022-05-16 08:55:43 -0300

    test: ensure AuthMiddleware return 403 if LoadAccountByToken returns null

--- src/presentation/middlewares/auth-middleware.spec.ts
+++ src/presentation/middlewares/auth-middleware.spec.ts
@@ -30,8 +30,15 @@ const makeSut = (): Middleware => new AuthMiddleware(loadAccountByTokenStub)
 describe('Auth MIddleware', () => {
   it('should return 403 if no x-access-token existss in header', async () => {
     const sut = makeSut()
-
     const httpResponse = await sut.handle({})
+    expect(httpResponse.statusCode).toBe(403)
+    expect(httpResponse.body).toEqual(new AccessDeniedError())
+  })
+
+  it('should return 403 if LoadAccountByToken return null', async () => {
+    const sut = makeSut()
+    jest.spyOn(loadAccountByTokenStub, 'loadByToken').mockResolvedValueOnce(null)
+    const httpResponse = await sut.handle(makeFakeRequest())
     expect(httpResponse.statusCode).toBe(403)
     expect(httpResponse.body).toEqual(new AccessDeniedError())
   })
--- src/presentation/middlewares/auth-middleware.ts
+++ src/presentation/middlewares/auth-middleware.ts
@@ -10,7 +10,9 @@ export class AuthMiddleware implements Middleware {
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     const accessToken = httpRequest.headers?.['x-access-token']
-    if (!accessToken) return forbidden(new AccessDeniedError())
-    await this.loadAccountByToken.loadByToken(accessToken)
+    if (accessToken) {
+      await this.loadAccountByToken.loadByToken(accessToken)
+    }
+    return forbidden(new AccessDeniedError())
   }
 }
