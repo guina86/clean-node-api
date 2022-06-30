96299ba90eacd47e9972c9cff2fdf4142e90e9fc
Leandro Guina <guina86@gmail.com>
2022-06-29 23:53:50 -0300

    refactor: change DbLoadSurveyById test mocks

--- tests/data/usecases/db-load-survey-by-id.spec.ts
+++ tests/data/usecases/db-load-survey-by-id.spec.ts
@@ -1,29 +1,40 @@
-import { DbLoadSurveyById } from '../../../src/data/usecases'
-import { mockLoadSurveyByIdRepository } from '../mocks'
-import { mockSurveyModel } from '../../domain/mocks'
-
-const loadSurveyByIdRepositoryStub = mockLoadSurveyByIdRepository()
-
-const makeSut = (): DbLoadSurveyById => new DbLoadSurveyById(loadSurveyByIdRepositoryStub)
+import { DbLoadSurveyById } from '@data/usecases'
+import { LoadSurveyByIdRepository } from '@data/protocols'
+import { mockSurveyModel } from '@tests/domain/mocks'
+import { mock } from 'jest-mock-extended'
 
 describe('DbLoadSurveyById', () => {
+  const makeSut = (): DbLoadSurveyById => new DbLoadSurveyById(loadSurveyByIdRepositorySpy)
+  const loadSurveyByIdRepositorySpy = mock<LoadSurveyByIdRepository>()
+
+  beforeAll(() => {
+    loadSurveyByIdRepositorySpy.loadById.mockResolvedValue(mockSurveyModel())
+  })
+
+  beforeEach(jest.clearAllMocks)
+
   it('should call LoadSurveyByIdRepository with correct id', async () => {
     const sut = makeSut()
-    const loadByIdSpy = jest.spyOn(loadSurveyByIdRepositoryStub, 'loadById')
+
     await sut.load('any_id')
-    expect(loadByIdSpy).toHaveBeenCalledWith('any_id')
+
+    expect(loadSurveyByIdRepositorySpy.loadById).toHaveBeenCalledWith('any_id')
   })
 
   it('should return survey on success', async () => {
     const sut = makeSut()
+
     const survey = await sut.load('id_1')
+
     expect(survey).toEqual(mockSurveyModel())
   })
 
   it('should throw if LoadSurveyByIdRepository throws', async () => {
+    loadSurveyByIdRepositorySpy.loadById.mockRejectedValueOnce(new Error())
     const sut = makeSut()
-    jest.spyOn(loadSurveyByIdRepositoryStub, 'loadById').mockRejectedValueOnce(new Error())
+
     const promise = sut.load('any_id')
+
     await expect(promise).rejects.toThrow()
   })
 })
